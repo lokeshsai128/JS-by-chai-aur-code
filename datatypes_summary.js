1cf3438182cbff5b9e0996682d9177e4b6fe1227
lokeshsai128 <lokeshsai128@gmail.com>
2024-08-27 07:12:40 +0530

    added datatypes_summary

--- datatypes_summary.js
+++ datatypes_summary.js
@@ -1,36 +1,62 @@
-// primitive 
+// // primitive 
 
-//7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+// //7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
-const bigNumber = 2345678934568976543456n
+// const bigNumber = 2345678934568976543456n
 
 
-// Reference (Non primitive)
+// // Reference (Non primitive)
 
-// Array, Objects, Functions
+// // Array, Objects, Functions
 
-const heros = ["shaktiman","heman","krish"]
-let myObj = {
-    name : "Lokesh",
-    age : 21,
-}
+// const heros = ["shaktiman","heman","krish"]
+// let myObj = {
+//     name : "Lokesh",
+//     age : 21,
+// }
 
-const myFunction = function(){
-    console.log("Hello world!");
-}
+// const myFunction = function(){
+//     console.log("Hello world!");
+// }
 
-console.log(typeof heros)
+// console.log(typeof heros)
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//+++++++++++++++++Stack and Heap memory in java++++++++++++++++++++++++++
+
+// Stack (Primitive) - just takes the values and copies it store in another stack
+// , Heap(Non-Primitive) - just reference from the heap where the data is stored and if changes are made, it is effected to every occurence
+
+
+let myYoutubename = "hiteshchoudary.com"
+
+let anotherName = myYoutubename
+anotherName = "chaiaurcode"
+
+console.log(myYoutubename );
+console.log(anotherName)
+
+let userOne = {
+    email  : "user@google.com",
+    upiId : "user@ybl"
+}
+ 
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
