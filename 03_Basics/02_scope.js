99192d4ecba95258d97b7ba99d40a17bac43dc27
lokeshsai128 <lokeshsai128@gmail.com>
2024-09-02 11:32:22 +0530

    added scopes with some more examples

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -5,9 +5,43 @@ let a = 100;
 if(true){
     let a = 10;
     const b = 20;
-    console.log("Inner : ",a);
+    // console.log("Inner : ",a);
     
 }
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "lokesh";
+    function two(){
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+}
+// one()
+
+if(true) {
+    const username = "lokesh";
+    if( username === "lokesh"){
+        const website = "youtube";
+        // console.log(username + " "+ website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// ++++++++++++++++ interesting +++++++++++++
+
+console.log(addOne(5));
+function addOne(num){
+    return num+1;
+}
+
+console.log(addTwo(5));
+const addTwo = function(num){
+    return num+2;
+}
