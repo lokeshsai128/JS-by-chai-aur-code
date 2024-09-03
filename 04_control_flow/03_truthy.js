28caefda6932d52f1df181a368a6d6d4a449661f
lokeshsai128 <lokeshsai128@gmail.com>
2024-09-03 22:36:43 +0530

    updated 03_truthy.js

--- 04_control_flow/03_truthy.js
+++ 04_control_flow/03_truthy.js
@@ -18,6 +18,23 @@ else{
 const emptyObj = {}
 
 if(Object.keys(emptyObj).length === 0){
-    console.log("Object is empty");
-    
+    console.log("Object is empty");   
 }
+
+
+// Nullish Coalescing Operator (??): null defined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ??20
+
+console.log(val1);
+
+//Terniary operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <=80 ? console.log("less than 80") : console.log("more then 80");
