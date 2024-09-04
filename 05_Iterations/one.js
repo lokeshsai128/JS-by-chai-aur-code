0a75d5887c6a255db1ac1871f071c6c3d7d44b0e
lokeshsai128 <lokeshsai128@gmail.com>
2024-09-04 16:21:11 +0530

    05_Iterations/one:

--- 05_Iterations/one.js
+++ 05_Iterations/one.js
@@ -1,21 +1,27 @@
-//for 
+// //for 
 
-for (let i = 0; i <= 10; i++) {
-    const element = i;
-    if(element == 5){
-        console.log("5 is best");
-    }
-    console.log(i);   
-}
-// console.log(i);
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if(element == 5){
+//         console.log("5 is best");
+//     }
+//     console.log(i);   
+// }
+// // console.log(i);
 
-for (let i = 1; i < 10+1 ; i++) {
-    // console.log(`Outer loop value : ${i}`);
-    console.log("------------------");
-    console.log("Table : "+i);
-    console.log("------------------");
-    for (let j = 1; j < 10+1; j++) {
-        // console.log(`Inner loop value ${j} of outerloop ${i}`);
-        console.log(i + " x " + j+" = "+ i*j);
-    }
+// for (let i = 1; i < 10+1 ; i++) {
+//     // console.log(`Outer loop value : ${i}`);
+//     console.log("------------------");
+//     console.log("Table : "+i);
+//     console.log("------------------");
+//     for (let j = 1; j < 10+1; j++) {
+//         // console.log(`Inner loop value ${j} of outerloop ${i}`);
+//         console.log(i + " x " + j+" = "+ i*j);
+//     }
+// }
+
+let myArray = ["ironman","thor","captain america"];
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
 }
