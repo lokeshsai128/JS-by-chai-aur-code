b2110e53f17a951b337ec7dc422d5eedf510e411
lokeshsai128 <lokeshsai128@gmail.com>
2024-09-04 15:06:37 +0530

    05_Iterations/one:

--- 05_Iterations/one.js
+++ 05_Iterations/one.js
@@ -11,6 +11,9 @@ for (let i = 0; i <= 10; i++) {
 
 for (let i = 1; i < 10+1 ; i++) {
     // console.log(`Outer loop value : ${i}`);
+    console.log("------------------");
+    console.log("Table : "+i);
+    console.log("------------------");
     for (let j = 1; j < 10+1; j++) {
         // console.log(`Inner loop value ${j} of outerloop ${i}`);
         console.log(i + " x " + j+" = "+ i*j);
