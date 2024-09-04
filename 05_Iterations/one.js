e6fdff4bbb88e4db7c1adf88a218b366c4032c9a
lokeshsai128 <lokeshsai128@gmail.com>
2024-09-04 14:52:55 +0530

    05_Iterations/one:

--- 05_Iterations/one.js
+++ 05_Iterations/one.js
@@ -2,7 +2,10 @@
 
 for (let i = 0; i <= 10; i++) {
     const element = i;
+    if(element == 5){
+        console.log("5 is best");
+    }
     console.log(i);   
 }
 
-console.log(i);
+// console.log(i);
