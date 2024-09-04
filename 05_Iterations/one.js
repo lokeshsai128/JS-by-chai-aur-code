e837204fb0dcf143ed1684fc3c936c57c911ab0c
lokeshsai128 <lokeshsai128@gmail.com>
2024-09-04 15:02:08 +0530

    commit

--- 05_Iterations/one.js
+++ 05_Iterations/one.js
@@ -9,10 +9,10 @@ for (let i = 0; i <= 10; i++) {
 }
 // console.log(i);
 
-for (let i = 0; i < 10 ; i++) {
-    console.log(`Outer loop value : ${i}`);
-    for (let j = 0; j < 10; j++) {
+for (let i = 1; i < 10+1 ; i++) {
+    // console.log(`Outer loop value : ${i}`);
+    for (let j = 1; j < 10+1; j++) {
         // console.log(`Inner loop value ${j} of outerloop ${i}`);
-        
+        console.log(i + " x " + j+" = "+ i*j);
     }
 }
