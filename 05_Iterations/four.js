54d3772e8eb37254ae029eed30b52f3ea91d62d9
lokeshsai128 <lokeshsai128@gmail.com>
2024-09-07 22:07:58 +0530

    05_Iterations/four:

--- 05_Iterations/four.js
+++ 05_Iterations/four.js
@@ -5,5 +5,11 @@ const myObject = {
     py : "python"
 }
 for (const key in myObject) {
-    console.log(`${key} is shortcut for ${myObject[key]}`);
+    // console.log(`${key} is shortcut for ${myObject[key]}`);
+}
+
+const programming = ['c++','python','java','javascript'];
+
+for (const key in programming) {
+    console.log(programming[key]);   
 }
