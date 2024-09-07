359f7fd87702aa8eb36be83a5a2b0ecca80c6f89
lokeshsai128 <lokeshsai128@gmail.com>
2024-09-07 22:11:27 +0530

    05_Iterations/four:

--- 05_Iterations/four.js
+++ 05_Iterations/four.js
@@ -11,5 +11,15 @@ for (const key in myObject) {
 const programming = ['c++','python','java','javascript'];
 
 for (const key in programming) {
-    console.log(programming[key]);   
+    // console.log(programming[key]);   
+}
+
+// The map is not iterable by using for in looping method
+const map = new Map();
+map.set("IN","India")
+map.set("UK","United Kindom")
+map.set("FR","France")
+
+for (const key in map) {
+    console.log(key);
 }
