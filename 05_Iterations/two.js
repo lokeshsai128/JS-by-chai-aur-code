05a712f5940b1c52bb2f0702fbc02da23e14008b
lokeshsai128 <lokeshsai128@gmail.com>
2024-09-07 21:32:54 +0530

    05_Iterations/two:

--- 05_Iterations/two.js
+++ 05_Iterations/two.js
@@ -1,7 +1,12 @@
-let index = 0;
-while (index<=10) {
-    console.log(`The value of index is : ${index}`);
-    index +=2;
-}
+// let index = 0;
+// while (index<=10) {
+//     console.log(`The value of index is : ${index}`);
+//     index +=2;
+// }
 
-let myArray = ["superman"]
+let myArray = ["superman","batman","aquaman"];
+let arr = 0;
+while(arr < myArray.length){
+    console.log(`The superhero is : ${myArray[arr]}`);
+    arr++;
+}
