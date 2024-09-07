8460dbef1d9430d85c3fb96a6f725bdc6b4575de
lokeshsai128 <lokeshsai128@gmail.com>
2024-09-07 21:47:26 +0530

    05_Iterations/three:

--- 05_Iterations/three.js
+++ 05_Iterations/three.js
@@ -3,8 +3,13 @@
 // ["","",""] -> we can have strings inside an array
 // [{},{},{}] -> we can also have objects inside an array which can be accessed by using either dot notation or []-brackets
 
-const arr = [1,2,3,4,5];
+// const arr = [1,2,3,4,5];
 
-for(const num of arr){
-    console.log(num); 
+// for(const num of arr){
+//     console.log(num); 
+// }
+
+const greetings = "Hello macha!";
+for (const element of greetings) {
+    console.log(`Each char in greetings : ${element}`);
 }
