e93702df5fb1a4570514a27901737d74128d28df
lokeshsai128 <lokeshsai128@gmail.com>
2024-09-10 16:39:04 +0530

    05_Iterations/six:

--- 05_Iterations/six.js
+++ 05_Iterations/six.js
@@ -1,7 +1,15 @@
-const coding = ['js','python','java','c++','ruby']
+// const coding = ['js','python','java','c++','ruby']
 
-const values = coding.forEach((item) => {
-    console.log(item);
-})
+// const values = coding.forEach((item) => {
+//     console.log(item);
+//     return item;
+// })
 
-console.log(values);
+// console.log(values);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+// const newNums = myNums.filter( (num) => num>4)
+const newNums = myNums.filter( (num) => {        //if scope is started then we must use the return keyword to return the objects
+   return num>4
+});
+console.log(newNums);
